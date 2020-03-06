34ce1ccd563c1e2e082518ed851185135c7f649e
unknown <pekoff.jp@gmail.com>
2020-03-06 22:15:23 +0300

    finished registration

---
+++ client/src/components/layout/Landing.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Landing = () => {
+  return (
+    <section className="landing">
+      <div className="dark-overlay">
+        <div className="landing-inner">
+          <h1 className="x-large">User Register Form</h1>
+          <p className="lead">Create profile, load, deleta and share files</p>
+          <div className="buttons">
+            <Link to="/register" className="btn btn-primary">
+              Sign Up
+            </Link>
+            <Link to="/login" className="btn btn-light">
+              Login
+            </Link>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Landing;
